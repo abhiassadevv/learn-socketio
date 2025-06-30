9de063d593b635ec5dc3a2257bb92e1a67da071e
abhiassadevv <abhiassadevv@gmail.com>
2025-06-30 16:35:52 +0700

    First push

---
+++ index.js
@@ -0,0 +1,23 @@
+const {createServer} = require('http');
+
+const {Server} = require('socket.io');
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:8158'
+  }
+});
+
+io.on('connection', (socket) => {
+  socket.emit('main', 'Welcome');
+  
+  socket.on('message', (msg) => {
+    console.log(msg);
+    
+    socket.emit('message', msg);
+  });
+});
+
+httpServer.listen(8000);
